af9ab126e105072de168ea29223dd9f03a7377d3
Lidia Parkhomova <slipinuk@gmail.com>
2024-08-01 23:43:23 +0200

    hw3 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+let operand1 = prompt("Введіть перший операнд:");
+let operand2 = prompt("Введіть другий операнд:");
+
+operand1 = parseInt(operand1,10);
+operand2 = parseInt(operand2, 10);
+
+console.log(`Результат: ${operand1} + ${operand2} = ${operand1 + operand2}`);
+console.log(`Результат: ${operand1} - ${operand2} = ${operand1 - operand2}`);
+console.log(`Результат: ${operand1} * ${operand2} = ${operand1 * operand2}`);
+console.log(`Результат: ${operand1} / ${operand2} = ${operand1 / operand2}`);
