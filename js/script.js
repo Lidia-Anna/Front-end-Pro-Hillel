6ae6b105c22141afe0fe15c2e40d918a56af5126
Lidia Parkhomova <slipinuk@gmail.com>
2024-08-01 15:18:03 +0200

    hw2 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 'use strict';
+// Task 1
+let admin;
+const name = "John";
+admin = name;
+alert(admin);
+
+//Task 2
+let ourPlanet;
+let nameConsumer;
+
+//Task 3
+let variable = "10" + 10;
+console.log(typeof variable);
+
+
