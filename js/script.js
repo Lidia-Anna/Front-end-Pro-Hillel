0621dec44843eb8dc19577d33053eeef5500024b
Lidia Parkhomova <slipinuk@gmail.com>
2024-08-04 23:51:35 +0200

    hw6 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+//Task 1
+let name= prompt("Як Вас звати?");
+let age = prompt("Скільки Вам років?");
+let likesProgramming = confirm('Вам подобається програмувати?');
+
+//Task 2
+console.log(name);
+console.log(age);
+console.log(likesProgramming);
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof likesProgramming);
+
+//Task 3
+let greeting = `Привіт, ${name}!`;
+alert(greeting);
+
+//Task 4
+let numericAge = Number(age);
+let nextAge = numericAge + 1;
+console.log(nextAge);
+
+//Task 5
+let isAdult = numericAge >= 18;
+console.log(isAdult);
