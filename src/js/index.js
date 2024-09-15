de3d6f04c9101478b6f337d2aa58cd377ebdaa4c
Lidia Parkhomova <slipinuk@gmail.com>
2024-09-15 23:11:34 +0200

    hw24 complited

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+function examplHosting() {
+  for (let i = 0; i < 3; i++) {
+    var a = 10;
+    const b = 20;
+    const c = 5;
+    console.log(`Value c in for ${c}`);
+  }
+  console.log(`Value a (var) = ${a}`);
+  console.log(b);
+  console.log(c);
+}
+examplHosting();
