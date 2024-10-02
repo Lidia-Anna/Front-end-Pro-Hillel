5f5d843924e4cf8298effee7af9be81bd9a889a5
Lidia Parkhomova <slipinuk@gmail.com>
2024-10-02 20:09:46 +0200

    hw33 fixed

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,13 @@
-const rendomNumber = () => {
-  const num = Math.floor(Math.random() * 10);
-  if (num === 0) return 1;
-  return num;
+'use strict';
+
+const randomNumber = () => {
+  const number = Math.floor(Math.random() * 10);
+  if (number === 0) return 1;
+  return number;
 };
 const generateImg = (imgName) => {
   const img = document.createElement('img');
   img.src = './img/' + `${imgName}` + '.png';
   return img;
 };
-document.body.append(generateImg(rendomNumber()));
+document.body.append(generateImg(randomNumber()));
