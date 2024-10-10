18aee2987dedce5f3c76cc3814c50fe6b5144eee
Lidia Parkhomova <slipinuk@gmail.com>
2024-10-10 23:05:22 +0200

    hw36 completed

---
+++ src/js/index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+(() => {
+  const LIST_TYPES = {
+    UL: 'ul',
+    OL: 'ol',
+    MENU: 'menu',
+  };
+
+  const appendHTMLElement = (parentEl = null, elementToAppend = null) => {
+    if (!parentEl || !elementToAppend) return;
+    parentEl.append(elementToAppend);
+  };
+  const generateList = (listItems, listType = LIST_TYPES.UL) => {
+    if (!Object.values(LIST_TYPES).includes(listType)) throw new Error('listType arguments must be a List type');
+    if (!listItems || !Array.isArray(listItems)) throw new Error('Array arguments must be a List array');
+    if (!listItems.length) throw new Error("array length shouldn't be a 0");
+
+    const listElement = document.createElement(listType);
+
+    listItems.forEach((item) => {
+      const listItemElement = document.createElement('li');
+      appendHTMLElement(listElement, listItemElement);
+
+      if (Array.isArray(item)) {
+        appendHTMLElement(listItemElement, generateList(item, listType));
+      } else {
+        listItemElement.innerHTML = item;
+      }
+      appendHTMLElement(listElement, listItemElement);
+    });
+    return listElement;
+  };
+  const result = generateList([1, 2, 3, [4, 5, 6]], 'ol');
+  appendHTMLElement(document.body, result);
+})();
