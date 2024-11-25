6139fb08f902f308489f96a7e00d58bc375edab9
Lidia Parkhomova <slipinuk@gmail.com>
2024-11-25 21:54:25 +0100

    hw45 added

---
+++ src/js/index.js
@@ -0,0 +1,34 @@
+class TaskQueue {
+// valueDelay = 1000;
+  arrTask = [];
+  constructor(valueDelay) {
+    this.valueDelay = valueDelay;
+  }
+    delay(ms) {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+    addTask(task) {
+        this.arrTask.push(task);
+    }
+
+    run() {
+        let promiseTask = Promise.resolve();
+
+        this.arrTask.forEach((task) => {
+            promiseTask = promiseTask
+                .then(() => {
+                    console.log(task);
+                    return this.delay(this.valueDelay);
+                });
+        });
+
+        promiseTask.then(() => {
+            console.log('Усі завдання завершено');
+        });
+    }
+}
+const queue = new TaskQueue(1000);
+queue.addTask('Task 1 виконано');
+queue.addTask('Task 2 виконано');
+queue.addTask('Task 3 виконано');
+queue.run();
